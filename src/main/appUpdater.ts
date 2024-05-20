fc4192bb0726ebc64e3117dc8319ca3995eb3c21
AdamUhh <adamuhh.dev@gmail.com>
2024-05-20 15:41:40 +0400

    v0.3.04

--- src/main/appUpdater.ts
+++ src/main/appUpdater.ts
@@ -17,21 +17,27 @@ class ApplicationUpdater {
 
     autoUpdater.autoInstallOnAppQuit = false;
 
-    // ? not really required since updates are downloaded automatically
-    // autoUpdater.on('update-available', () => log.info('update-available'));
-
     autoUpdater.on('error', (err) => {
       sendToRenderer(IpcChannels.appUpdateInfo, {
         success: false,
         msg: 'Error in app updater',
         description: err == null ? 'unknown' : (err.stack || err).toString(),
       });
-      // dialog.showErrorBox(
-      //   'Error in auto-updater',
-      //   err == null ? 'unknown' : (err.stack || err).toString(),
-      // );
     });
 
+    // ? not really required since updates are downloaded automatically
+    // autoUpdater.on('update-available', () => log.info('update-available'));
+
+    /** No updates available */
+    autoUpdater.on('update-not-available', () => {
+      sendToRenderer(IpcChannels.appUpdateInfo, {
+        success: false,
+        msg: 'Update',
+        description: 'No updates found',
+      });
+    });
+
+    /** Download Completion Message */
     autoUpdater.on(
       'update-downloaded',
       async ({ releaseName, releaseNotes }) => {
@@ -40,24 +46,6 @@ class ApplicationUpdater {
           msg: releaseName || '',
           description: (releaseNotes as string) || '',
         });
-
-        // Show the message box with formatted release notes
-        // dialog
-        //   .showMessageBox({
-        //     title: 'Update Available',
-        //     type: 'question',
-        //     detail: `${releaseName || ''}\n${releaseNotes || ''}`,
-        //     message: 'Update available. Install now and restart?',
-        //     buttons: ['Install and Relaunch', 'Later'],
-        //     defaultId: 0,
-        //     cancelId: 1,
-        //   })
-        //   .then((result) => {
-        //     if (result.response === 0) autoUpdater.quitAndInstall();
-        //   })
-        //   .catch((error) => {
-        //     log.info(error);
-        //   });
       },
     );
 
@@ -65,25 +53,8 @@ class ApplicationUpdater {
   }
 
   /** User manually checks for an update */
-  public static async checkForUpdates(): Promise<void> {
-    try {
-      const updateResult = await autoUpdater.checkForUpdates();
-      if (!updateResult)
-        // dialog.showMessageBox({
-        //   title: 'Update',
-        //   type: 'info',
-        //   message: 'No updates found',
-        //   buttons: ['Okay'],
-        //   cancelId: 0,
-        // });
-        this.sendToRenderer(IpcChannels.appUpdateInfo, {
-          success: false,
-          msg: 'Update',
-          description: 'No updates found',
-        });
-    } catch (error) {
-      console.log('Error checking for updates:', error);
-    }
+  public static async checkForUpdates() {
+    autoUpdater.checkForUpdates();
   }
 
   public static async quitAndInstall() {
